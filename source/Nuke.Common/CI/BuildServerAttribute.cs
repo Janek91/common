2ea8bc4b8a3bb16fcd0d3fcd7909a88231369b37
Matthias Koch <ithrowexceptions@gmail.com>
2019-10-28 13:53:25 +0100

    Add BuildServer injection

--- source/Nuke.Common/CI/BuildServerAttribute.cs
+++ source/Nuke.Common/CI/BuildServerAttribute.cs
@@ -4,13 +4,23 @@
 
 using System;
 using System.Linq;
+using System.Reflection;
 using JetBrains.Annotations;
+using Nuke.Common.Execution;
 
 namespace Nuke.Common.CI
 {
     [MeansImplicitUse(ImplicitUseTargetFlags.WithMembers)]
-    [AttributeUsage(AttributeTargets.Class)]
-    public class BuildServerAttribute : Attribute
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Field | AttributeTargets.Property)]
+    public class BuildServerAttribute : InjectionAttributeBase
     {
+        public override object GetValue(MemberInfo member, object instance)
+        {
+            var memberType = member.GetMemberType();
+            var instanceProperty = memberType.GetProperty(nameof(TeamCity.TeamCity.Instance), ReflectionService.Static);
+            ControlFlow.Assert(instanceProperty != null,
+                $"Type '{memberType}' is not compatible for injection via '{nameof(BuildServerAttribute)}'.");
+            return instanceProperty.GetValue(obj: null);
+        }
     }
 }
